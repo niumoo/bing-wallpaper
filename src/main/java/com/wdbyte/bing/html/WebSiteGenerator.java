670e7e9b2aeea15c872e2f05b68672906c4b0cc7
niumoo <niumoo@126.com>
2025-01-07 22:11:03 +0800

    feat(WebSiteGenerator): 更新今日图片生成逻辑并增加JSON格式输出
    此提交更新了今日图片生成逻辑，将输出格式从简单的字符串改为JSON对象，并增加了更多元数据字段如日期和地区等。同时采用了新的Fastjson库进行JSON处理。

--- src/main/java/com/wdbyte/bing/html/WebSiteGenerator.java
+++ src/main/java/com/wdbyte/bing/html/WebSiteGenerator.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter.Feature;
 
 import com.wdbyte.bing.BingFileUtils;
 import com.wdbyte.bing.Images;
@@ -50,8 +52,14 @@ public class WebSiteGenerator {
 
     private void htmlGeneratorToday(List<Images> bingImages) throws IOException {
         String url = bingImages.get(0).getUrl();
-        String fileName = String.format("%s-%s.jpg", Wallpaper.CURRENT_REGION, bingImages.get(0).getDate());
-        HtmlFileUtils.writeToday(fileName + ":" + url);
+        String fileName = String.format("%s_%s.jpg", Wallpaper.CURRENT_REGION, bingImages.get(0).getDate());
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("file_name", fileName);
+        jsonObject.put("url", url);
+        jsonObject.put("date", bingImages.get(0).getDate());
+        jsonObject.put("region", Wallpaper.CURRENT_REGION);
+        jsonObject.put("desc", bingImages.get(0).getDesc());
+        HtmlFileUtils.writeToday(jsonObject.toString(Feature.PrettyFormat));
     }
 
     public void htmlGeneratorIndex(List<Images> bingImages, Map<String, List<Images>> monthMap) throws IOException {
